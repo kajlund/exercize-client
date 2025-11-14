a22dfd1056304c2ce486c20f3eff1579797eec00
Kaj Lund <kaj.lund@gmail.com>
2025-11-14 17:22:16 +0200

    Adds axios calls to backend for dynamic list of activities

---
+++ src/services/ActivityServices.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:4000/api/v1';
+const apiClient = axios.create({
+  baseURL: API_URL,
+  withCredentials: false,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+async function fetchActivities() {
+  try {
+    const response = await apiClient.get('/activities');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching activities:', error);
+    throw error;
+  }
+}
+
+export default {
+  fetchActivities,
+}
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,20 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
+import ActivityService from '@/services/ActivityServices.js';
 import ActivityCard from '@/components/ActivityCard.vue';
 
-const activities = ref([
-  {
-    id: "68f90685b3a2cc203afc5872",
-    when: "2025-10-22T00:00:00.000Z",
-    kind: "Outdoor walking",
-    title: "Afternoon walk",
-    description: "Nice walk in Hamnskogen",
-    distance: 4.72,
-    duration: 3391,
-    elevation: 72,
-    calories: 0,
-    createdAt: "2025-10-22T16:29:57.159Z",
-    updatedAt: "2025-10-22T16:29:57.159Z"
+const activities = ref(null);
+
+onMounted(async () => {
+  try {
+    const response = await ActivityService.fetchActivities();
+    console.log(response.data);
+    activities.value = response.data;
+  } catch (error) {
+    console.error('Error fetching activities:', error);
   }
-])
+});
 </script>
 
 <template>
